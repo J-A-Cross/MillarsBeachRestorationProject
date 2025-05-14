88edf13e6c1ab451fc67ad66e250134693f38d68
J-A-Cross <jamcro007@gmail.com>
2025-05-14 20:43:44 +1200

    Responsive Navbar

---
+++ MillarsBeachRestorationProjectViteProject/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+<nav className="navbar">
+    <div className="navbar-top">
+        <h1 className="navbar-logo">Millars Beach Restoration Project</h1>
+        <div className="navbar-menu-icon" onClick={toggleMenu}>☰</div>
+    </div>
+    <ul className={`navbar-links ${isOpen ? "navbar-links-open" : ""}`}>
+        <li><a href="#ecological-report">Ecological Report</a></li>
+        <li><a href="#donate">Donate</a></li>
+        <li><a href="#our-updates">Our Updates</a></li>
+        <li><a href="#contact-us">Contact Us</a></li>
+        <li><a href="#sign-in">Sign In</a></li>
+    </ul>
+</nav>
+
+    );
+};
+
+export default Navbar;
