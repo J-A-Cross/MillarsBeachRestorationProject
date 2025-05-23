2d84d61087cc4daf9cbc53096422318822f14ff2
J-A-Cross <jamcro007@gmail.com>
2025-05-24 09:36:29 +1200

    Updates

--- MillarsBeachRestorationProjectViteProject/src/Components/Donate/Donate.jsx
+++ MillarsBeachRestorationProjectViteProject/src/Components/Donate/Donate.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import './Donate.css';
 
 const Donate = () => {
   return (
--- MillarsBeachRestorationProjectViteProject/src/Components/Posts/Posts.jsx
+++ MillarsBeachRestorationProjectViteProject/src/Components/Posts/Posts.jsx
@@ -1,25 +1,100 @@
 import React from 'react';
-import Post from '../Post/Post';
 import './Posts.css';
 
 const Posts = () => {
-  const dummyText = `Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissenti eos cu eum an brute copiosae hendrerit.`;
-
-  const postList = [
-    { title: 'Post Title', date: 'Date / Time', location: 'Location Details Here', content: dummyText, imageLeft: true },
-    { title: 'Post Title', date: 'Date / Time', location: 'Location Details Here', content: dummyText, imageLeft: false },
-  ];
 
   return (
-    <div className="posts-wrapper">
-      <h1 className="updates-heading">Our Updates</h1>
-      {postList.map((post, index) => (
-        <Post key={index} {...post} />
-      ))}
-      <div className="load-more-container">
-        <button className="load-more-button">Load More Posts</button>
-      </div>
+<div className="bg-[url('https://upload.wikimedia.org/wikipedia/commons/c/c8/Altja_j%C3%B5gi_Lahemaal.jpg')] bg-cover bg-center bg-no-repeat bg-fixed">
+  <div>
+  <h1 className="mx-15 py-15 text-4xl font-bold text-amber-50">Our Posts</h1>
+  <div className="mx-15 my-10 flex h-auto flex-row rounded-2xl bg-blue-800/80 text-white">
+    <div className="m-5 flex h-auto max-w-md rounded-2xl">
+      <img className="rounded-2xl h-auto w-full" src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=3200&quality=80" alt="Description of image" />
     </div>
+
+    <div>
+      <div className="mx-5 mt-5 flex flex-row flex-wrap">
+        <h1 className="mx-2 font-semibold">Conservation Project</h1>
+        <h3 className="mx-2">21st Febrary 2024</h3>
+      </div>
+      <div className="mx-5">
+        <p className="mx-2">*Millars Beach, Stewart Isalnd NZ</p>
+      </div>
+      <p className="mx-7 mt-5 mb-8 pr-2">I think the nature of the first half of the semester means that there is not excessive amounts of practical activity, but that it is ok as there is a proper time and places for different learning types during study, and the flow during this course is logical and we as students can see the direction we are going (Which is not always the case with other classNamees).</p>
+    </div>
+  </div>
+
+  <div className="mx-15 my-10 flex h-auto flex-row rounded-2xl bg-blue-800/80 text-white">
+    <div>
+      <div className="mx-5 mt-5 flex flex-row flex-wrap">
+        <h1 className="mx-2 font-semibold">Conservation Project</h1>
+        <h3 className="mx-2">21st Febrary 2024</h3>
+      </div>
+      <div className="mx-5">
+        <p className="mx-2">*Millars Beach, Stewart Isalnd NZ</p>
+      </div>
+      <p className="mx-7 mt-5 mb-8 pr-2">I think the nature of the first half of the semester means that there is not excessive amounts of practical activity, but that it is ok as there is a proper time and places for different learning types during study, and the flow during this course is logical and we as students can see the direction we are going (Which is not always the case with other classNamees).</p>
+    </div>
+    <div className="m-5 flex h-auto max-w-md rounded-2xl">
+      <img className="rounded-2xl h-auto w-full" src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=3200&quality=80" alt="Description of image" />
+    </div>
+
+
+  </div>
+
+  <div className="mx-15 my-10 flex h-auto flex-row rounded-2xl bg-blue-800/80 text-white">
+    <div className="m-5 flex h-auto max-w-md rounded-2xl">
+      <img className="rounded-2xl h-auto w-full" src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=3200&quality=80" alt="Description of image" />
+    </div>
+
+    <div>
+      <div className="mx-5 mt-5 flex flex-row flex-wrap">
+        <h1 className="mx-2 font-semibold">Conservation Project</h1>
+        <h3 className="mx-2">21st Febrary 2024</h3>
+      </div>
+      <div className="mx-5">
+        <p className="mx-2">*Millars Beach, Stewart Isalnd NZ</p>
+      </div>
+      <p className="mx-7 mt-5 mb-8 pr-2">I think the nature of the first half of the semester means that there is not excessive amounts of practical activity, but that it is ok as there is a proper time and places for different learning types during study, and the flow during this course is logical and we as students can see the direction we are going (Which is not always thethat there is not excessive amounts of practical activity, but that it is ok as there is a proper time and places for different learning types during study, and the flow during this course is logical and we as students can see the direction we are going (Which is not always the case with other classNamees). case with other classNamees).</p>
+    </div>
+  </div>
+
+  <div className="mx-15 my-10 flex h-auto flex-row rounded-2xl bg-blue-800/80 text-white">
+    <div>
+      <div className="mx-5 mt-5 flex flex-row flex-wrap">
+        <h1 className="mx-2 font-semibold">Conservation Project</h1>
+        <h3 className="mx-2">21st Febrary 2024</h3>
+      </div>
+      <div className="mx-5">
+        <p className="mx-2">*Millars Beach, Stewart Isalnd NZ</p>
+      </div>
+      <p className="mx-7 mt-5 mb-8 pr-2">I think the nature of the first half of the semester means that there is not excessive amounts of practical activity, but that it is ok as there is a proper time and places for different learning types during study, and the flow during this course is logical and we as students can see the direction we are going (Which is not always the case with other classNamees).</p>
+    </div>
+    <div className="m-5 flex h-auto max-w-md rounded-2xl">
+      <img className="rounded-2xl h-auto w-full" src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=3200&quality=80" alt="Description of image" />
+    </div>
+
+
+  </div>
+
+  <div className="mx-15 my-10 flex h-auto flex-row rounded-2xl bg-blue-800/80 text-white">
+    <div className="m-5 flex h-auto max-w-md rounded-2xl">
+      <img className="rounded-2xl h-auto w-full" src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=3200&quality=80" alt="Description of image" />
+    </div>
+
+    <div>
+      <div className="mx-5 mt-5 flex flex-row flex-wrap">
+        <h1 className="mx-2 font-semibold">Conservation Project</h1>
+        <h3 className="mx-2">21st Febrary 2024</h3>
+      </div>
+      <div className="mx-5">
+        <p className="mx-2">*Millars Beach, Stewart Isalnd NZ</p>
+      </div>
+      <p className="mx-7 mt-5 mb-8 pr-2">I think the nature of the first half of the semester means that there is not excessive amounts of practical activity, but that it is ok as there is a proper time and places for different learning types during study, and the flow during this course is logical and we as students can see the direction we are going (Which is not always the case with other classNamees).that there is not excessive amounts of practical activity, but that it is ok as there is a proper time and places for different learning types during study, and the flow during this course is logical and we as students can see the direction we are going (Which is not always the case with other classNamees).</p>
+    </div>
+  </div>
+  </div>
+</div>
   );
 };
 
